032d9b4324d449a9937472a508399f49b2fa6d58
adprasad1996 <adprasad1996@gmail.com>
2025-06-14 22:35:00 +0530

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./assets/components/Navbar";
 import Footer from "./assets/components/Footer";
-import Home from "../src/assets/pages/Home"
-import AboutPage from "../src/assets/pages/AboutPage";
-import ProjectsPage from "../src/assets/pages/ProjectsPage";
-import ContactPage from "../src/assets/pages/ContactPage";
-
+import Home from "./assets/pages/Home";
+import AboutPage from "./assets/pages/AboutPage";
+import ProjectsPage from "./assets/pages/ProjectsPage";
+import ContactPage from "./assets/pages/ContactPage";
 
 function App() {
   return (
-    <Router>
+    <Router >
       <Navbar />
       <div className="pt-20"></div>
-      <Routes>        
-        <Route path="https://adprasad1996.github.io/PrasadPortFolio/" element={<Home/>}/>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
-       <Footer />
+      <Footer />
     </Router>
   );
 }
